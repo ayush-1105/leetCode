b8ecbb0eb15c320ae9fadf4b62ce3baf48e3356c
ayush-1105 <104482219+ayush-1105@users.noreply.github.com>
2023-12-18 16:40:45 +0530

    Time: 467 ms (42.86%), Space: 7.6 MB (88.57%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.c
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.c
@@ -1,9 +1,17 @@
 int maxProductDifference(int* nums, int numsSize){
-    for (int i = 0; i < numsSize - 1; i++)
-        for (int j = 0; j < numsSize - i - 1; j++)
-            if (nums[j] > nums[j + 1])
-                // Swap elements if they are in the wrong order
-                nums[j] ^= nums[j + 1] ^= nums[j] ^= nums[j + 1];
+    for (int i = 1; i < numsSize; i++) {
+        int key = nums[i];
+        int j = i - 1;
 
+        // Move elements greater than key to one position ahead
+        while (j >= 0 && nums[j] > key) {
+            nums[j + 1] = nums[j];
+            j = j - 1;
+        }
+
+        nums[j + 1] = key;
+    }
+
+    // Calculate the maximum product difference
     return nums[numsSize - 1] * nums[numsSize - 2] - nums[0] * nums[1];
 }
