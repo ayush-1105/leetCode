fdfdeef72ec584f1bdb31454f17e9634f14ed024
ayush-1105 <104482219+ayush-1105@users.noreply.github.com>
2023-12-20 22:02:53 +0530

    Time: 9 ms (75.45%), Space: 46.9 MB (58.30%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        int min1=INT_MAX, min0=INT_MAX;
-        for(int x: prices){
-            if (x<min0) 
-                min1=exchange(min0, x);
-            else min1=min(min1, x);
-        }
-        int left=money-min0-min1;
-        return (left>=0)?left:money;
+        // int min1=INT_MAX, min0=INT_MAX;
+        // for(int x: prices){
+        //     if (x<min0) 
+        //         min1=exchange(min0, x);
+        //     else min1=min(min1, x);
+        // }
+        // int left=money-min0-min1;
+        // return (left>=0)?left:money;
+        nth_element(prices.begin(), prices.begin()+1, prices.end());
+       int left=money-prices[0]-prices[1];
+       return (left>=0)?left:money;
     }
 };
