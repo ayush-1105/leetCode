b15be338350c7127befec1275a264afef6012ec6
ayush-1105 <104482219+ayush-1105@users.noreply.github.com>
2024-01-18 22:37:43 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (71.72%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,18 +8,28 @@ public:
 //         //it is generalised. Instead of 4 we have n (which we return directly), instead of 2 we have n - 2, instead of 3 we have n - 1
 //         return climbStairs(n - 2) + climbStairs(n - 1);
         
-        
-        int dp[n + 1]; //save total ways to reach 0 to n, so when we need to reach some value that we already calculated, we don't need to calculate it again
-        dp[0] = 1; // total ways to reach 0
-        dp[1] = 1; // total ways to reach 1
+    // 2nd way    
+//         int dp[n + 1]; //save total ways to reach 0 to n, so when we need to reach some value that we already calculated, we don't need to calculate it again
+//         dp[0] = 1; // total ways to reach 0
+//         dp[1] = 1; // total ways to reach 1
 
-        //if n < 2 we don't need to do any calculation
+//         //if n < 2 we don't need to do any calculation
+//         if (n >= 2)
+//             for (int i = 2; i <= n; i++)
+//                 //the line of code above corresponde to `ways to reach 4 = (ways to reach 2) + (ways to reach 3)`
+//                 //it is generalised. Instead of 4 we have i, instead of 2 we have i - 2, instead of 3 we have i - 1
+//                 dp[i] = dp[i - 2] + dp[i - 1];
+
+//         return dp[n];
+        //3rd way
+        int dp[3];
+        dp[0] = 1;
+        dp[1] = 1;
+
         if (n >= 2)
             for (int i = 2; i <= n; i++)
-                //the line of code above corresponde to `ways to reach 4 = (ways to reach 2) + (ways to reach 3)`
-                //it is generalised. Instead of 4 we have i, instead of 2 we have i - 2, instead of 3 we have i - 1
-                dp[i] = dp[i - 2] + dp[i - 1];
+                dp[i % 2] = dp[(i - 1) % 2] + dp[(i - 2) % 2];
 
-        return dp[n];
+        return dp[n % 2];
     }
 };
